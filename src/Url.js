ab72ca789226caaa83cfe4f1703cda8ca5f0bf46
Mohammad Munaf <mohammadmehfooz100@gmail.com>
2024-08-22 17:35:52 +0530

    fix url

--- src/Url.js
+++ src/Url.js
@@ -1,2 +1,2 @@
-export const baseUrl = `https://ecommerce-client-ivory-gamma.vercel.app`;
+export const baseUrl = `https://ecommerceserver-s6ql.onrender.com`;
 //export const baseUrl = `http://localhost:3001`
